76b6b7c76a4c278608bd83b7d2a5a4a5ef2d909d
mortal-city <amirmahdi.ghalenoii@gmail.com>
2017-08-05 13:34:13 +0430

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -239,7 +239,7 @@ function create_config( )
     "time",
     "welcome"
     },
-    sudo_users = {308073797},--Sudo users
+    sudo_users = {313338151},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
